f46b58ca7a7a6852dcc914339af911ec8b88d21d
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-08-12 12:29:58 +0100

    Outlined first portion of Startup.rb

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,34 @@
 class Startup
+    attr_accessor :name
+    attr_reader :founder, :domain
+    @@all = []
+
+    def initialize(name:, founder:, domain:)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self
+    end
+
+    def pivot(domain:, name:) #string of domain and name
+        #change the domain and name of the startup. 
+        #This is the only public method through which the domain can be changed.
+        @domain = domain
+        @name = name
+        self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_founder(founder)
+        self.all.find {|startup| startup.founder == founder}
+    end
+
+    def self.domains
+        self.all.map {|startup| startup.domain}
+    end
+    
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,16 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+#name, founder, domain
+my_super_business = Startup.new(name: "Super Business", founder: "Erin", domain: "tech")
+bens_business = Startup.new(name: "Great Business", founder: "Ben", domain: "tech")
+business3 = Startup.new(name: "Business 3", founder: "Neil", domain: "healthcare")
+
+
+
+#my_super_business.pivot(domain: "health", name: "SUPER")
+
+
 binding.pry
+puts "end test"
 0 #leave this here to ensure binding.pry isn't the last line
