b535129c28abd83b8a56fe53b8f7bf47b6328b03
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-08-12 14:00:43 +0100

    Done.

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -16,5 +16,28 @@ class VentureCapitalist
         self.all.select {|vc| vc.total_worth > 1000000000}
     end
 
+    def offer_contract(startup, type, amount)
+        #given a startup object
+        #return type of investment as a strin
+        FundingRound.new(startup:startup, venture_capitalist: self, type:type, investment:amount)
+    end
+
+    def portfolio
+        FundingRound.all.select {|fr| fr.venture_capitalist == self}
+    end
+
+    def funding_rounds
+        portfolio.count
+    end
+
+    def biggest_investment
+        portfolio.max_by {|investment| investment.investment}
+    end
+
+    def invested(domain)
+        portfolio.select {|investment| investment.startup.domain == domain}.map{|domain| domain.investment}.reduce(:+)
+    end
+
+    
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -13,8 +13,16 @@ bens_business = Startup.new(name: "Great Business", founder: "Ben", domain: "tec
 business3 = Startup.new(name: "Business 3", founder: "Neil", domain: "healthcare")
 
 
+vc1 = VentureCapitalist.new(name: "Figaro", total_worth: 1000000001)
+vc2 = VentureCapitalist.new(name: "PoBoy", total_worth: 100)
+
+fr1 = FundingRound.new(startup: my_super_business, venture_capitalist:vc1, type: "Angel", investment: 10000.00)
+fr2 = FundingRound.new(startup: bens_business, venture_capitalist:vc2, type: "Pre-Seed", investment: 99.00)
+
+
 
 #my_super_business.pivot(domain: "health", name: "SUPER")
+my_super_business.sign_contract(vc2, "angel", 200)
 
 
 binding.pry
